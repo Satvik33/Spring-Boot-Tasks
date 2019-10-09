739eec162aa0b2c80a095d68c9b8ea64b83414fe
satvikadarsh.kumar <satvikadarsh.kumar@cgi.com>
2019-10-09 11:10:22 +0530

    task9

--- src/main/java/com/stackroute/userservice/domain/Movie.java
+++ src/main/java/com/stackroute/userservice/domain/Movie.java
@@ -1,54 +1,21 @@
 package com.stackroute.userservice.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Movie {
     @Id
     int movieId;
     String movieName;
     String movieContent;
-
-    public Movie() {
-    }
-
-    public Movie(int movieId, String movieName, String movieContent) {
-        this.movieId = movieId;
-        this.movieName = movieName;
-        this.movieContent = movieContent;
-    }
-
-    public int getMovieId() {
-        return movieId;
-    }
-
-    public void setMovieId(int movieId) {
-        this.movieId = movieId;
-    }
-
-    public String getMovieName() {
-        return movieName;
-    }
-
-    public void setMovieName(String movieName) {
-        this.movieName = movieName;
-    }
-
-    public String getMovieContent() {
-        return movieContent;
-    }
-
-    public void setMovieContent(String movieContent) {
-        this.movieContent = movieContent;
-    }
-
-    @Override
-    public String toString() {
-        return "Movie{" +
-                "movieId=" + movieId +
-                ", movieName='" + movieName + '\'' +
-                ", movieContent='" + movieContent + '\'' +
-                '}';
-    }
 }
